cc3bd29a4aec5e60a7cb0c9a84d79f4d2819c68d
askalmas <ask.almas@gmail.com>
2020-10-12 15:14:20 +0200

    gitignore

--- config.py
+++
@@ -1,4 +0,0 @@
-KAFKA_CONSUME_TOPICS = ["detections", "detections-utm", "objects-utm", "fusion-utm", "fusion-inout-objects-utm",
-                        "fusion-inout-utm", "objects", "campus-st", "dummycontrol"]
-KAFKA_HOST = '10.3.100.166'
-KAFKA_PORT = '9093'

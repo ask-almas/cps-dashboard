23512f8fdbc023c873adc46aacf0c2e57d44de9a
askalmas <ask.almas@gmail.com>
2021-02-01 16:34:00 +0100

    merge with updated version

--- cps_dashboard.py
+++ cps_dashboard.py
@@ -1,39 +1,119 @@
-from config.config import KAFKA_HOST, KAFKA_PORT, KAFKA_CONSUME_TOPICS
+import time
+
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 from dash.dependencies import Input, Output
-from kafka_messages_counter import KafkaMessagesCounter
+from dash_extensions import Keyboard
 
+from config.config import SYMBOLS, OPACITY, COLORS, SIZES
+from kafka_client import KafkaClient
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    html.H4(
-        'Kafka Topic Messages'
-    ),
-    html.Table([
-        html.Thead(
-            html.Tr([html.Th(topic) for topic in KAFKA_CONSUME_TOPICS])
-        ),
-        html.Tbody(id='table-records')
-    ]),
-    dcc.Interval(
-        id='interval-component',
-        interval=1000,
-        n_intervals=0
-    )
+app = dash.Dash()
+obj_list = [[]]
+obj_idx = 0
+old_topic = None
+
+app.layout = html.Div(children=[
+    Keyboard(id="keyboard"),
+    html.H1("Central Perception Dashboard"),
+    dcc.Dropdown(id="topic-choice"),
+    dcc.Interval(id='interval_component', interval=1000, n_intervals=0),
+    html.Div(id="output"),
+    dcc.Graph(id="bev_map", animate=False)
 ])
 
 
-@app.callback(Output('table-records', 'children'),
-              [Input('interval-component', 'n_intervals')])
-def update_records(n):
-    kmc = KafkaMessagesCounter(KAFKA_HOST, KAFKA_PORT, KAFKA_CONSUME_TOPICS)
-    msgs = kmc.topics_messages_count()
-    return [html.Tr(
-        [html.Td(mas) for mas in msgs]
-    )]
+@app.callback(Output('topic-choice', 'options'), Input('interval_component', 'n_intervals'))
+def update_options(n):
+    kafka_client = KafkaClient()
+    return kafka_client.get_topic_options()
+
+
+@app.callback([Output("output", "children"), Output('bev_map', 'figure')],
+              [Input("keyboard", "keydown"), Input("keyboard", "n_keydowns"), Input("topic-choice", "value")])
+def keydown(event, n_keydowns, value):
+    global obj_list, obj_idx, old_topic
+
+    if value is None:
+        scene = get_curr_scene()
+        return scene
+
+    is_utm = False
+    if 'utm' in value:
+        is_utm = True
+
+    if value != old_topic:
+        old_topic = value
+        obj_idx = 0
+        obj_list = [[]]
+        obj_list = update_obj_list(value, is_utm)
+        scene = get_curr_scene(is_utm)
+        print("done")
+        return scene
+
+    if event is not None and event["key"] == "ArrowRight":
+        if obj_idx < len(obj_list) - 1:
+            obj_idx += 1
+    if event is not None and event["key"] == "ArrowLeft":
+        if obj_idx > 0:
+            obj_idx -= 1
+
+    scene = get_curr_scene(is_utm)
+    return scene
+
+
+def update_obj_list(value, is_utm=False):
+    kafka_client = KafkaClient()
+    return kafka_client.get_messages_from_topic(value, is_utm)
+
+
+def get_curr_scene(is_utm=False):
+    global obj_list, obj_idx
+    scene_objects = obj_list[obj_idx]
+    # TODO: maybe somehow it can be avoided to create new figure object for each step
+    fig = go.Figure()
+    fig.update_layout(
+        showlegend=True,
+        autosize=False,
+        width=900,
+        height=900
+    )
+
+    if not is_utm:
+        fig.update_xaxes(range=[19.05583, 19.05697])
+        fig.update_yaxes(range=[47.47845, 47.47925])
+    else:
+        fig.update_xaxes(range=[353520.38, 353608.49])
+        fig.update_yaxes(range=[5260166.73, 5260253.49])
+
+    if len(scene_objects) > 0 and scene_objects[-1] is not None:
+        data_sources = {}
+        for so in scene_objects:
+            if so.ssid not in data_sources:
+                data_sources[so.ssid] = []
+            if so.suid >= 0:
+                data_sources[so.ssid].append(so)
+        for source_system in data_sources:
+            ds_objects = data_sources[source_system]
+            fig.add_trace(go.Scatter(
+                x=[o.lon for o in ds_objects],
+                y=[o.lat for o in ds_objects],
+                marker=dict(color=COLORS[source_system], size=SIZES[source_system]),
+                opacity=OPACITY[source_system],
+                mode="markers+text",
+                text=[f"{o.cov_xx:.3f}     {o.cov_yy:.3f}" for o in ds_objects] if source_system != "3_390" else "",
+                marker_symbol=SYMBOLS[source_system],
+                name=source_system
+            ))
+        object_nums = {}
+        for source_system in data_sources:
+            ds_objects = data_sources[source_system]
+            object_nums[source_system] = len(ds_objects)
+        return f"{old_topic} Fusion time @ {scene_objects[-1].ts / 1000.0} {object_nums}", fig
+    else:
+        return f"No data in future! (requested fusion step: {obj_idx})", fig
 
 
 if __name__ == '__main__':
---
+++ kafka_client.py
@@ -0,0 +1,127 @@
+import time
+
+from kafka import KafkaConsumer
+from kafka.structs import TopicPartition
+
+from config.config import KAFKA_PORT, KAFKA_HOST, KAFKA_CONSUME_TOPICS, SENSORIS_WGS_RESOLUTION, FUSION_SSID, \
+    PRE_FUSION_TOPICS
+from gps_object import GPSObject
+from sensoris.protobuf.messages.data_pb2 import DataMessage
+
+
+class KafkaClient(object):
+    def __init__(self):
+        self._kafka_host = KAFKA_HOST
+        self._kafka_port = KAFKA_PORT
+        self._kafka_topics = KAFKA_CONSUME_TOPICS
+        self._consumer = None
+
+    def get_topic_options(self):
+        count = self.count_messages_in_topics()
+        res = []
+        for i, t in enumerate(self._kafka_topics):
+            res.append({"label": f"{t}: {count[i]} messages available", "value": t})
+        return res
+
+    def count_messages_in_topics(self):
+        res = []
+        self._subscribe_for_topics()
+        for topic in self._kafka_topics:
+            r = self._count_messages_in_topic(topic)
+            res.append(r)
+        self._stop()
+        return res
+
+    def _subscribe_for_topics(self):
+        self._consumer = KafkaConsumer(bootstrap_servers=f'{self._kafka_host}:{self._kafka_port}',
+                                       auto_offset_reset='earliest',
+                                       consumer_timeout_ms=1000)
+        self._consumer.subscribe(self._kafka_topics)
+
+    def _count_messages_in_topic(self, topic):
+        partitions = self._count_topic_partitions(topic)
+        topic_messages = 0
+        if not partitions:
+            return 0
+        for partition in partitions:
+            topic_messages += self._count_messages_in_partition(topic, partition)
+        return topic_messages
+
+    def _count_topic_partitions(self, topic):
+        return list(self._consumer.partitions_for_topic(topic))
+
+    def _count_messages_in_partition(self, topic, partition_id):
+        topic_partition = [TopicPartition(topic, partition_id)]
+        beginning_offset = self._consumer.beginning_offsets(topic_partition)
+        end_offset = self._consumer.end_offsets(topic_partition)
+        return end_offset[topic_partition[0]] - beginning_offset[topic_partition[0]]
+
+    def _stop(self):
+        self._consumer.close()
+
+    def get_messages_from_topic(self, topic, is_utm=False):
+        tic = time.perf_counter()
+        print(f"Retrieving messages from topic: {topic}")
+        self._connect_to_topic(topic)
+        objects_list = [[]]
+        self._consumer.poll()
+        d = {}
+        pre_fusion = True if topic in PRE_FUSION_TOPICS else False
+        for msg in self._consumer:
+            dm = DataMessage()
+            dm.ParseFromString(msg.value)
+            objects = self._gps_objects_from(dm, is_utm)
+            if pre_fusion:
+                if objects[0].ssid in d:
+                    objects_list.append([])
+                    d = {}
+                else:
+                    d[objects[0].ssid] = objects[0].ssid
+                objects_list[-1].extend(objects)
+            else:
+                objects_list[-1].extend(objects)
+                if objects[0].ssid == FUSION_SSID:
+                    objects_list.append([])
+        self._stop()
+        toc = time.perf_counter()
+        print(f"Messages updated in {toc - tic:0.4f} seconds")
+        return objects_list
+
+    def _connect_to_topic(self, topic):
+        self._consumer = KafkaConsumer(topic,
+                                       bootstrap_servers=f'{self._kafka_host}:{self._kafka_port}',
+                                       consumer_timeout_ms=1000)
+        print(f"Kafka {topic} topic has partitions: {self._consumer.partitions_for_topic(topic)}")
+        self._consumer.seek_to_beginning()
+
+    def _gps_objects_from(self, dm, utm=False):
+        result = []
+        ssid = dm.envelope.ids.vehicle_id.value
+        origin = dm.event_group[0].envelope.origin
+        origin_ts = origin.timestamp.posix_time.value
+        origin_gps = origin.position_and_accuracy.metric_ecef if utm else origin.position_and_accuracy.geographic_wgs84
+        origin_lat, origin_lon, origin_alt = self._unpack_gps(origin_gps, utm)
+        origin = GPSObject(ssid, -1, origin_ts, origin_lat, origin_lon, origin_alt)
+        result.append(origin)
+        movable_objects = dm.event_group[0].object_detection_category.movable_object
+        for mov in movable_objects:
+            movobj_id = mov.object_id.value
+            movobj_conf = mov.existence_confidence.value / 100.0
+            box = mov.rectangular_box_and_accuracy.center_orientation_size
+            box_gps = box.center_position_and_accuracy.metric_ecef if utm else box.center_position_and_accuracy.geographic_wgs84
+            box_lat, box_lon, box_alt = self._unpack_gps(box_gps, utm)
+            cov_xx = box.center_position_and_accuracy.covariance.a11.value / 1000.0
+            cov_yy = box.center_position_and_accuracy.covariance.a22.value / 1000.0
+            cov_xy = box.center_position_and_accuracy.covariance.a12.value / 1000.0
+            movobj = GPSObject(ssid, movobj_id, origin_ts, box_lat, box_lon, box_alt, movobj_conf, cov_xx, cov_yy,
+                               cov_xy)
+            result.append(movobj)
+        return result
+
+    @staticmethod
+    def _unpack_gps(sensoris_gps_pos, utm=False):
+        lat = sensoris_gps_pos.y.value/1000.0 if utm else sensoris_gps_pos.latitude.value * SENSORIS_WGS_RESOLUTION
+        lon = sensoris_gps_pos.x.value/1000.0 if utm else sensoris_gps_pos.longitude.value * SENSORIS_WGS_RESOLUTION
+        alt = sensoris_gps_pos.z.value/1000.0 if utm else sensoris_gps_pos.altitude.value / 1000.0
+        print(lat, lon)
+        return lat, lon, alt
